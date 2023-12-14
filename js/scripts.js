482669f5d1ef3fdb05dc9ad65b67c8b7891460d0
Michel Henrique <michelstcosta@gmail.com>
2023-12-14 15:09:12 -0300

    acrescentando validações

--- js/scripts.js
+++ js/scripts.js
@@ -2,13 +2,23 @@ class Validator {
 
     constructor() {
         this.validations = [
+            'data-required',
             'data-min-length',
+            'data-max-length',
+            'data-email-validate'
         ];
     }
 
     // Iniciar a validação em todos os campos
 
     validate(form) {
+
+        let currentValidations = document.querySelectorAll("form .error-validation")
+
+        if (currentValidations.length > 0) {
+            this.currentValidations(currentValidations);
+        }
+
         let inputs = form.getElementsByTagName("input");
 
         // Transforma uma HTMLCollection e um Array
@@ -38,12 +48,42 @@ class Validator {
         let errorMessage = `O campo precisa ter pelo menos ${minValue} caracters`;
 
         if (inputLength < minValue) {
-            this.printMessage(input,errorMessage)
+            this.printMessage(input, errorMessage)
         }
 
     }
 
-    printMessage(input, msg){
+    maxlength(input, maxValue){
+        let inputLength = input.value.length;
+
+        let errorMessage = `O campo precisa ter menos que ${maxValue} caracters`;
+
+        if (inputLength < maxValue) {
+            this.printMessage(input, errorMessage)
+        }
+
+    }
+
+    emailvalidate(input){
+        
+        let re = /\S+@\S+\.\S+/;
+
+        let email = input.value;
+
+        let errorMessage = "Insira um e-mail no padrão email@email.com";
+
+        if(!re.test(email)){
+            this.printMessage(input,errorMessage);
+        }
+    }
+    
+
+
+    printMessage(input, msg) {
+        let errorQty = input.parentNode.querySelector(".error.validation")
+        
+        if(errorQty === null) {
+            
         let template = document.querySelector(".error-validation").cloneNode(true);
 
         template.textContent = msg;
@@ -53,10 +93,23 @@ class Validator {
         template.classList.remove("template");
 
         inputParent.appendChild(template);
+        }
 
     }
 
+    required(input){
+        let inputValue = input.value;
 
+        if(inputValue === ""){
+            let errorMessage = "Este campo é Obrigatorio";
+
+            this.printMessage(input, errorMessage);
+        }
+    }
+
+    cleanValidations(validations){
+        validations.forEach(el => el.remove());
+    }
 
 }
 
